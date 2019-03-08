1a4e99cc5569d11da14635186bdd6287141a9d2a
sachinOraon <sachinoraon55@gmail.com>
2019-03-09 00:44:34 +0530

    Image display ui

--- filters/blur_php/blur_avg.php
+++ filters/blur_php/blur_avg.php
@@ -10,18 +10,35 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<link href="../../css/bootstrap.min.css" rel="stylesheet">
+	<style>
+		.img-box
+		{
+			box-shadow: 0 8px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+			padding:10px;
+		}
+	</style>
 </head>
 <body>
-	<div class="container">
+	<div class="container-fluid">
+		<div class="row img-box" style="margin:10px;">
+			<div class="col-md-10"><h2>Image Processing</h2></div>
+			<div class="col-md-2">
+			<a href="../../filters.php" class="btn btn-primary" role="button">Back</a>
+			<a href="../../index.php" class="btn btn-success" role="button">Home</a>
+			</div>
+		</div><br><br>
 		<div class="row">
-			<div class="col-md-6">
-				<h3 class="text-center">Before</h3>
-				<?php echo '<img src=\'../../images/abc.jpg\' class=\'img-thumbnail img-fluid\'>'; ?>
+			
+			<div class="col-md-5 img-box" style='margin-left:40px'>
+				<h3 class="text-center" style='font-family:Tw cen mt'>Original</h3>
+				<?php echo '<img src=\'../../images/abc.jpg\' class=\'rounded img-fluid\'>'; ?>
 			</div>
-			<div class="col-md-6">
-				<h3 class="text-center">After Averaging</h3>
-				<?php echo '<img src=\'../../images/abc_blur_avg.jpg\' class=\'img-thumbnail img-fluid\'>'; ?>
+			<div class="col-md-1"></div>
+			<div class="col-md-5 img-box">
+				<h3 class="text-center" style='font-family:Tw cen mt'>After Averaging</h3>
+				<?php echo '<img src=\'../../images/abc_blur_avg.jpg\' class=\'rounded img-fluid\'>'; ?>
 			</div>
+			<div class="col-md-2"></div>
 		</div>
 	</div>
 </body>
